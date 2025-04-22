5a4d09f760b64a9e98f2a2384d3e427ce1f163a5
Jean Patrick <jean_patrick115@hotmail.com>
2025-04-22 13:51:08 -0300

    starting backend

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
